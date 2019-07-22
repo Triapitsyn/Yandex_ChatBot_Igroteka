4396039c8c409eda546f1002c855135ed379dd9f
Tryapitsyn <atryapa@gmail.com>
2019-07-22 22:16:21 +0300

    initial

--- main_function.py
+++ main_function.py
@@ -6,19 +6,19 @@ import little_fuctions
 
 aliceAnswers = read_answers_data("data/answers_dict_example")
 
-def message_return(response, user_storage, text, speech, buttons, database):
+def message_return(response, user_storage, text, speech, buttons, user_id, database):
     response.set_text(text)
     response.set_tts(speech)
     user_storage["suggests"] = buttons
     buttons, user_storage = little_fuctions.get_suggests(user_storage)
     response.set_buttons(buttons)
-    database.update_entries('users_info', id, {'last_text': text}, update_type='rewrite')
-    database.update_entries('users_info', id, {'last_speech': speech}, update_type='rewrite')
-    database.update_entries('users_info', id, {'last_buttons': buttons}, update_type='rewrite')
+    database.update_entries('users_info', user_id, {'last_text': text}, update_type='rewrite')
+    database.update_entries('users_info', user_id, {'last_speech': speech}, update_type='rewrite')
+    database.update_entries('users_info', user_id, {'last_buttons': buttons}, update_type='rewrite')
     return response, user_storage
 
-def idk_return(response, user_storage):
-    last_text, last_speech, last_buttons = little_fuctions.get_lasts(id, database)
+def user_idk_return(response, user_storage, user_id, database):
+    last_text, last_speech, last_buttons = little_fuctions.get_lasts(user_id, database)
     text = 'Я вас не поняла, давайте попробуем еще раз.\n{}'.format(last_text)
     speech = 'Я вас не поняла, давайте попробуем еще раз.'.format(last_speech)
     buttons = last_buttons
@@ -32,32 +32,32 @@ def idk_return(response, user_storage):
 
 def handle_dialog(request, response, user_storage, database):
 
-    if not database.get_entry("users_info", ['new'], {'request_id': request.user_id}):
-        database.add_entries("users_info", {"request_id": request.user_id})
+    if not database.get_entry("users_info", ['new'], {'request_user_id': request.user_id}):
+        database.add_entries("users_info", {"request_user_id": request.user_id})
     if not user_storage:
         user_storage = {"suggests": []}
 
     input = request.command
-    id = request.user_id
-    isfirsttime = request.is_new_session
-    if isfirsttime:
+    user_id = request.user_id
+    is_first_time = request.is_new_session
+    if is_first_time:
         mode = ''
-        little_fuctions.update_mode(id, mode, database)
+        little_fuctions.update_mode(user_id, mode, database)
     else:
-        mode = little_fuctions.get_mode(id, database)
+        mode = little_fuctions.get_mode(user_id, database)
 
     if mode.startswith('yesno') or (mode == '' and little_fuctions.isequal(input, 'Данетки')):
         import yes_no_puzzle
-        succes = yes_no_puzzle.start(response, id, database)
+        succes = yes_no_puzzle.start(response, user_id, database)
         if succes:
-            message_return(response, user_storage, *succes)
+            message_return(response, user_storage, *succes, user_id, database)
         else:
-            idk_return(response, user_storage)
+            user_idk_return(response, user_storage, user_id, database)
     elif mode == '' and input == '':
         text = 'Привет, выбери игру'
         speech = text
         buttons = 'Данетки'
-        message_return(response, user_storage, text, speech, buttons)
+        message_return(response, user_storage, text, speech, buttons, user_id, database)
     else:
-        alice_interaction.idk(id, database)
+        user_idk_return(response, user_storage, user_id, database)
         pass
