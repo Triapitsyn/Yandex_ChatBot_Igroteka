e629b1a10b34b5b0559cd73dafc74295a44c7dfd
Tryapitsyn <atryapa@gmail.com>
2019-07-22 21:53:40 +0300

    initial

--- main_function.py
+++ main_function.py
@@ -8,13 +8,25 @@ aliceAnswers = read_answers_data("data/answers_dict_example")
 
 def message_return(response, user_storage, text, speech, buttons, database):
     response.set_text(text)
-    database.update_entries('users_info', id, {'last_text': text}, update_type='rewrite')
     response.set_tts(speech)
-    database.update_entries('users_info', id, {'last_speech': speech}, update_type='rewrite')
     user_storage["suggests"] = buttons
     buttons, user_storage = little_fuctions.get_suggests(user_storage)
     response.set_buttons(buttons)
+    database.update_entries('users_info', id, {'last_text': text}, update_type='rewrite')
+    database.update_entries('users_info', id, {'last_speech': speech}, update_type='rewrite')
     database.update_entries('users_info', id, {'last_buttons': buttons}, update_type='rewrite')
+    return response, user_storage
+
+def idk_return(response, user_storage):
+    last_text, last_speech, last_buttons = little_fuctions.get_lasts(id, database)
+    text = 'Я вас не поняла, давайте попробуем еще раз.\n{}'.format(last_text)
+    speech = 'Я вас не поняла, давайте попробуем еще раз.'.format(last_speech)
+    buttons = last_buttons
+    response.set_text(text)
+    response.set_tts(speech)
+    user_storage["suggests"] = buttons
+    buttons, user_storage = little_fuctions.get_suggests(user_storage)
+    response.set_buttons(buttons)
     return response, user_storage
 
 
@@ -33,12 +45,19 @@ def handle_dialog(request, response, user_storage, database):
         little_fuctions.update_mode(id, mode, database)
     else:
         mode = little_fuctions.get_mode(id, database)
-    last_text, last_speech, last_buttons = little_fuctions.get_lasts(id, database)
 
-    mode = little_fuctions.get_mode(id, database)
-    if mode.startswith('yesno') or (mode == '' and little_fuctions.isequal(response, 'Данетки')):
+    if mode.startswith('yesno') or (mode == '' and little_fuctions.isequal(input, 'Данетки')):
         import yes_no_puzzle
-        yes_no_puzzle.start(response, id, database)
+        succes = yes_no_puzzle.start(response, id, database)
+        if succes:
+            message_return(response, user_storage, *succes)
+        else:
+            idk_return(response, user_storage)
+    elif mode == '' and input == '':
+        text = 'Привет, выбери игру'
+        speech = text
+        buttons = 'Данетки'
+        message_return(response, user_storage, text, speech, buttons)
     else:
         alice_interaction.idk(id, database)
         pass
--- yes_no_puzzle.py
+++ yes_no_puzzle.py
@@ -1,61 +1,62 @@
-def start(text, id, database):
+def start(input, id, database):
     import little_fuctions
     mode = little_fuctions.get_mode(id, database)
     if not mode.startswith('yesno'):
-        return_start()
+        text, speech, buttons = return_start()
         mode = 'yesno>main'
-    elif mode == 'yesno>main' and little_fuctions.isequal(text, 'Правила'):
-        return_rules()
+    elif mode == 'yesno>main' and little_fuctions.isequal(input, 'Правила'):
+        text, speech, buttons = return_rules()
         mode = 'yesno>rules'
-    elif mode == 'yesno>rules' and little_fuctions.isequal(text, 'Начать'):
-        return_riddle(1)
+    elif mode == 'yesno>rules' and little_fuctions.isequal(input, 'Начать'):
+        text, speech, buttons = return_riddle(1)
         mode = 'yesno>riddle>1'
     elif mode.startswith('yesno>riddle>'):
         number = int(mode.split('>')[2])
-        if little_fuctions.isequal(text, 'Назад'):
-            return_riddle(number - 1)
+        if little_fuctions.isequal(input, 'Назад'):
+            text, speech, buttons = return_riddle(number - 1)
             mode = 'yesno>riddle>{}'.format(number - 1)
-        elif little_fuctions.isequal(text, 'Дальше'):
-            return_riddle(number + 1)
+        elif little_fuctions.isequal(input, 'Дальше'):
+            text, speech, buttons = return_riddle(number + 1)
             mode = 'yesno>riddle>{}'.format(number + 1)
-        elif little_fuctions.isequal(reponse.split()[0], 'Пропустить'):
-            if text.split[1].isdigit():
-                skip = int(text.split[1])
+        elif little_fuctions.isequal(input.split()[0], 'Пропустить'):
+            if input.split[1].isdigit():
+                skip = int(input.split[1])
             else:
                 skip = 1
-            return_riddle(number + skip)
+            text, speech, buttons = return_riddle(number + skip)
             mode = 'yesno>riddle>{}'.format(number + skip)
+        else:
+            return False
     else:
-        import alice_interaction
-        alice_interaction.idk(id, database)
+        return False
     little_fuctions.update_mode(id, mode, database)
-    pass
+    return text, speech, buttons
 
 
 def return_start():
-    import alice_interaction
-    alice_interaction.return_answer(buttons=['Начать', 'Правила', 'В начало'],
-                                    text='Для вашего удобства загадки будут представлены по возрастанию сложности.',
-                                    speech='Для вашего удобства загадки будут представлены по возрастанию сложности.')
-    return
+    text = 'Для вашего удобства загадки будут представлены по возрастанию сложности.'
+    speech = 'Для вашего удобства загадки будут представлены по возрастанию сложности.'
+    buttons = ['Начать', 'Правила', 'В начало']
+    return text, speech, buttons
 
 def return_rules():
-    import alice_interaction
-    alice_interaction.return_answer(buttons=['Начать', 'В начало'],
-                                    text='Ведущий описывает странную ситуацию, а угадывающие должны, задавая уточняющие вопросы, выяснить её. При угадывании можно задавать вопросы, но ответом на них могут быть только слова «да», «нет», «не имеет значения» или «не корректно».',
-                                    speech='Ведущий описывает странную ситуацию, а угадывающие должны, задавая уточняющие вопросы, выяснить её. При угадывании можно задавать вопросы, но ответом на них могут быть только слова «да», «нет», «не имеет значения» или «не корректно».')
-    return
+    text='Ведущий описывает странную ситуацию, а угадывающие должны, задавая уточняющие вопросы, выяснить её. При угадывании можно задавать вопросы, но ответом на них могут быть только слова «да», «нет», «не имеет значения» или «не корректно».'
+    speech='Ведущий описывает странную ситуацию, а угадывающие должны, задавая уточняющие вопросы, выяснить её. При угадывании можно задавать вопросы, но ответом на них могут быть только слова «да», «нет», «не имеет значения» или «не корректно».'
+    buttons=['Начать', 'В начало']
+    return text, speech, buttons
 
 def return_riddle(number):
     import alice_interaction
     import yes_no_puzzle_biblio
+
     warning = False
     if number > len(yes_no_puzzle_biblio.riddles):
         number = len(yes_no_puzzle_biblio.riddles)
         warning = 'Сегодня у нас только {} загадок, показываю последнюю.\n'
-    alice_interaction.return_answer(buttons=['Дальше', 'Назад', 'В начало'],
-                                    text='{}{}\nОтвет: {}'.format(str(number)+') ' if not warning else warning,
-                                                                    yes_no_puzzle_biblio.riddles[number],
-                                                                    yes_no_puzzle_biblio.answers[number]),
-                                    speech=yes_no_puzzle_biblio.riddles[number])
-    return
+
+    text='{}{}\nОтвет: {}'.format(str(number)+') ' if not warning else warning,
+                                yes_no_puzzle_biblio.riddles[number - 1],
+                                yes_no_puzzle_biblio.answers[number - 1])
+    speech=yes_no_puzzle_biblio.riddles[number - 1]
+    buttons=['Дальше', 'Назад', 'В начало']
+    return text, speech, buttons
