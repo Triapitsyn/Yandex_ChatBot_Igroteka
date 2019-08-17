7133869f9761c8833ee3ffcfc16c268e41ca6354
Tryapitsyn <atryapa@gmail.com>
2019-08-17 20:21:48 +0300

    initial

--- main_function.py
+++ main_function.py
@@ -23,7 +23,7 @@ def message_return(response, user_storage, text, speech, buttons, mode, user_id,
         user_storage["card"]["header"]["text"] = text
         response.set_card(user_storage["card"])
     elif mode == "yesno>main":
-        user_storage["card"] = yesno_card(little_fuctions.get_color(user_id, database))
+        user_storage["card"] = yesno_card(little_fuctions.get_color(user_id, database), user_id, database)
         user_storage["card"]["header"]["text"] = text
         response.set_card(user_storage["card"])
     elif mode == "croco>main":
@@ -70,7 +70,7 @@ def idk_return(response, user_storage, user_id, database, mode, comment = ''):
         user_storage["card"]["header"]["text"] = text
         response.set_card(user_storage["card"])
     elif mode == "yesno>main":
-        user_storage["card"] = yesno_card(little_fuctions.get_color(user_id, database))
+        user_storage["card"] = yesno_card(little_fuctions.get_color(user_id, database), user_id, database)
         response.set_card(user_storage["card"])
     elif mode == "croco>main":
         user_storage["card"] = croco_card(little_fuctions.get_color(user_id, database))
